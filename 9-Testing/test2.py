b2be75c35beafe94c351d7b57a7b991bdfa466b8
Prerak Panwar <40028120+prerakpanwar@users.noreply.github.com>
2022-09-04 11:02:14 +0530

    Add files via upload

---
+++ 9-Testing/test2.py
@@ -0,0 +1,23 @@
+import unittest
+import script2
+
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        # please note in the video, I had the parameters flipped it should be the "guess" parameter 1st and "answer" parameter 2nd
+        result = main.run_guess(5, 5)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        result = main.run_guess(0, )
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = main.run_guess(11, 5)
+        self.assertFalse(result)
+
+    def test_input_wrong_type(self):
+        result = main.run_guess('11', 5)
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
