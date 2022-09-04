8f980b794243aa3b0d4ede24c6266e07f08c0e81
Prerak Panwar <40028120+prerakpanwar@users.noreply.github.com>
2022-09-04 09:48:20 +0530

    Update sys_argv.py

--- 6-Modules/sys_argv.py
+++ 6-Modules/sys_argv.py
@@ -1,6 +1,7 @@
 # The sys module in Python provides various functions and variables that are used to manipulate different parts of
 # the Python runtime environment. It allows operating on the interpreter as it provides access to the variables and
 # functions that interact strongly with the interpreter.
+# To learn more about sys (https://www.knowledgehut.com/blog/programming/sys-argv-python-examples)
 import sys
 
 first = sys.argv[1]
