b3ea4e081e18f69c93178ad0f712ff6db919267d
Prerak Panwar <40028120+prerakpanwar@users.noreply.github.com>
2022-09-26 11:46:08 +0530

    Update email1.py

--- 10-Scripting/Email/email1.py
+++ 10-Scripting/Email/email1.py
@@ -1,32 +1,32 @@
-# import smtplib
+import smtplib
 
-# # creates SMTP session
-# s = smtplib.SMTP('smtp.gmail.com', 587)
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
  
-# # start TLS for security
-# s.starttls()
+# start TLS for security
+s.starttls()
  
-# # Authentication
-# s.login("erikyoung217@gmail.com", "pdzqxqsaubbbdknv")
+# Authentication
+s.login("sender's email", "password")
  
-# # message to be sent
-# message = "yup I sent this email via Python"
+# message to be sent
+message = "yup I sent this email via Python"
  
-# # sending the mail
-# s.sendmail("erikyoung217@gmail.com", "panwarprerak98@gmail.com", message)
+# sending the mail
+s.sendmail("sender's email", "receiver's email", message)
  
-# # terminating the session
-# s.quit()
+# terminating the session
+s.quit()
 
 
-# -----------------------------------------------------------------
+-----------------------------------------------------------------
 
 import smtplib
 from email.message import EmailMessage
 
 email = EmailMessage()
 email['from'] = 'Erik Young'
-email['to'] = 'panwarprerak98@gmail.com'
+email['to'] = 'receiver's email'
 email['subject'] = 'You won 1,000,000 dollars!'
 
 email.set_content('I am a python master')
@@ -34,6 +34,6 @@ email.set_content('I am a python master')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
   smtp.starttls()
-  smtp.login('erikyoung217@gmail.com', 'pdzqxqsaubbbdknv')
+  smtp.login('sender's email', 'password')
   smtp.send_message(email)
   print('all good boss!')
